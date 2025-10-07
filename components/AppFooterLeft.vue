befc77b287085bf039f4d16266bf199481d1ad4a
bos <112678332+SupaschaiPh@users.noreply.github.com>
2025-10-07 09:47:16 +0700

    chore:setup theme

---
+++ components/AppFooterLeft.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="text-sm text-muted">
+    {{ new Date().getFullYear() }} © SecSpace Is a property of SecSpace's Dev Team 
+    
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ["~/assets/css/main.css"],
   modules: [
     "@nuxt/content",
     "@nuxt/image",
     "@nuxt/scripts",
-    "@nuxt/ui"
-  ]
-})
+    "@nuxt/ui",
+    "@nuxt/fonts",
+  ],
+  github: false,
+  socials: {
+    github: "https://github.com/IT-CyberCTF-Platform",
+  },
+});
